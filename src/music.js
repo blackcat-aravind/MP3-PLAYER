6115d7106afdb113fd2a77bf13ec83db141cc4e8
blackcat-aravind <heysaiaravind@gmail.com>
2025-02-19 04:52:32 +0530

    Add files via upload

---
+++ src/music.js
@@ -0,0 +1,57 @@
+let allMusic = [
+    {
+        name: "Sarkar ： Jaura Phagwara",
+        artist: "JAURA PHAGWARA",
+        img: "Sarkar ： Jaura Phagwara",
+        src: "Sarkar ： Jaura Phagwara"
+    },
+
+    {
+        name: "Ishq",
+        artist: "FAHEM ABDULLAH",
+        img: "Ishq",
+        src: "Ishq"
+    },
+
+    {
+        name: "EMIWAY BANTAI - ZINDAGI MAST HAI",
+        artist: "EMIWAY BANTAI",
+        img: "EMIWAY BANTAI - ZINDAGI MAST HAI",
+        src: "EMIWAY BANTAI - ZINDAGI MAST HAI"
+    },
+
+    {
+        name: "DIVINE - MIRCHI",
+        artist: "DIVINE",
+        img: "DIVINE - MIRCHI",
+        src: "DIVINE - MIRCHI"
+    },
+
+    {
+        name: "VICTORY ANTHEM-",
+        artist: "LUSHCURRY & KHUSHI",
+        img: "VICTORY ANTHEM-",
+        src: "VICTORY ANTHEM-"
+    },
+
+    {
+        name: "Chhore NCR aale",
+        artist: "MC SQUARE & PARADOX",
+        img: "Chhore NCR aale",
+        src: "Chhore NCR aale"
+    },
+
+    {
+        name: "So High x Millionaire",
+        artist: "YO YO HONEY SINGH",
+        img: "So High x Millionaire",
+        src: "So High x Millionaire"
+    },
+
+    {
+        name: "BAARISHEIN",
+        artist: "ANUV JAIN",
+        img: "BAARISHEIN",
+        src: "BAARISHEIN"
+    },
+]
